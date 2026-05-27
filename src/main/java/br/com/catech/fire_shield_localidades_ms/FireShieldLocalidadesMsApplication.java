be15e4bd27af3df134b0e3cabdb4edca72f16c6b
jonasdasneves <jonas.joliveira2024@gmail.com>
2026-05-26 22:29:33 -0300

    (feat) Definição de dependências e estrutura básica do projeto

---
+++ src/main/java/br/com/catech/fire_shield_localidades_ms/FireShieldLocalidadesMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.catech.fire_shield_localidades_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireShieldLocalidadesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireShieldLocalidadesMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/catech/fire_shield_localidades_ms/FireShieldLocalidadesMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catech.fire_shield_localidades_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireShieldLocalidadesMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
